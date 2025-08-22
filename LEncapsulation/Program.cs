e66b70f97409332f0cda9e85164fd73cca3c4e29
ACT-STUDENT <ACT-STUDENT@LAB3-PC29>
2025-08-22 10:54:06 +0800

    Encapsulation

---
+++ LEncapsulation/Program.cs
@@ -0,0 +1,33 @@
+﻿using LEncapsulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Langit_EncapsulationUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt user to enter the brand and size  of the shirt
+            Console.Write("Enter Brand: ");
+            string brand = Console.ReadLine();
+            Console.Write("Enter Size: ");
+            string size = Console.ReadLine();
+
+            // Create a new instance of the Shirt class
+            Shirt shirt1 = new Shirt();
+
+            // Assign the user input to the Shirt's Brand  and size property
+            shirt1.Brand = brand;
+            shirt1.Size = size;
+
+            // Call the method to display the shirt's information
+            shirt1.DisplayInfo();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ LEncapsulation/Shirt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEncapsulation
+{
+    internal class Shirt
+    {
+        // Property to store the brand and size of the shirt
+        public string Brand {  get; set; }       
+        public string Size { get; set; }
+
+        // Method to display the information of the shirt
+        public void DisplayInfo()
+        {
+            Console.WriteLine(); // an empty line for formatting
+            // Output the brand and size of the shirt
+            Console.WriteLine($"Brand: { Brand }, Size: { Size } ");
+            
+        }
+    }
+}
