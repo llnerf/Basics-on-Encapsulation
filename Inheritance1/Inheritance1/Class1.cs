b28eb2e55e678bfa1cb2b1e1c17cc7e759520759
llnerf <langitlawrence3@gmail.com>
2025-09-03 09:23:36 +0800

    Update Class1.cs

--- Inheritance1/Inheritance1/Class1.cs
+++ Inheritance1/Inheritance1/Class1.cs
@@ -4,44 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Inheritance1
+namespace Langit_SimpleInheritance
 {
-   class Student
-{
-    //fields
-    public string Name { get; set; }
-    public string Program { get; set; }
-
-    // Method to display student's basic information (Name + Program)
-    public void BasicInfo()
+  
+    class Student
     {
-        Console.WriteLine("Name: " + Name);
-        Console.WriteLine("Program: " + Program);
+        //fields
+        public string Name { get; set; }
+        public string Program { get; set; }
+
+        // Method to display student's basic information (Name + Program)
+        public void BasicInfo()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Program: " + Program);
+        }
+    }
+    // Inherits from Student
+    class RegularStudent : Student
+    {
+        // Additional property specific to RegularStudent
+        public string Section { get; set; }
+
+        // Method to display section where the regular student is enrolled
+        public void SectionEnrolled()
+        {
+            Console.WriteLine("Section: " + Section);
+        }
+    }
+
+    // Inherits from Student
+    class IrregularStudent : Student
+    {
+        // Additional property specific to IrregularStudent
+        public int UnitsEnrolled { get; set; }
+
+        // Method to display total units enrolled by the irregular student
+        public void EnrolledSemUnits()
+        {
+            Console.WriteLine("Units Enrolled: " + UnitsEnrolled);
+        }
     }
 }
-// Inherits from Student
-class RegularStudent : Student
-{
-    // Additional property specific to RegularStudent
-    public string Section { get; set; }
 
-    // Method to display section where the regular student is enrolled
-    public void SectionEnrolled()
-    {
-        Console.WriteLine("Section: " + Section);
-    }
-}
-
-// Inherits from Student
-class IrregularStudent : Student
-{
-    // Additional property specific to IrregularStudent
-    public int UnitsEnrolled { get; set; }
-
-    // Method to display total units enrolled by the irregular student
-    public void EnrolledSemUnits()
-    {
-        Console.WriteLine("Units Enrolled: " + UnitsEnrolled);
-    }
-}
-}
